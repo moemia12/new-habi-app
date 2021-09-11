9142bf620359266c8cd7c47cc9f3073a34054fd6
Mohsin Miah <moemia12@gmail.com>
2021-09-12 00:51:03 +0100

    App Commit

---
+++ constants/images.js
@@ -0,0 +1,44 @@
+export const plant1 = require("../assets/images/plant_1.jpeg");
+export const plant2 = require("../assets/images/plant_2.jpg");
+export const plant3 = require("../assets/images/plant_3.jpg");
+export const plant4 = require("../assets/images/plant_4.jpg");
+export const plant5 = require("../assets/images/plant_5.jpg");
+export const plant6 = require("../assets/images/plant_6.jpg");
+export const plant7 = require("../assets/images/plant_7.jpg");
+export const profile1 = require("../assets/images/profile_1.jpg");
+export const profile2 = require("../assets/images/profile_2.jpg");
+export const profile3 = require("../assets/images/profile_3.jpg");
+export const profile4 = require("../assets/images/profile_4.jpg");
+export const profile5 = require("../assets/images/profile_5.jpg");
+export const bannerBg = require("../assets/images/banner_bg.jpg");
+export const lizard = require("../assets/images/lizard.jpg");
+export const lizard2 = require("../assets/images/lizard2.jpg");
+export const lizard3 = require("../assets/images/lizard3.jpg");
+export const lizard4 = require("../assets/images/lizard4.jpg");
+export const turtle = require("../assets/images/turtle.jpg");
+export const rat = require("../assets/images/rat.png");
+export const spider = require("../assets/images/spider.jpg");
+
+
+export default {
+    plant1,
+    plant2,
+    plant3,
+    plant4,
+    plant5,
+    plant6,
+    plant7,
+    profile1,
+    profile2,
+    profile3,
+    profile4,
+    profile5,
+    bannerBg,
+    lizard,
+    turtle,
+    rat,
+    spider,
+    lizard2,
+    lizard3,
+    lizard4
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+ import {AppRegistry} from 'react-native';
+ import App from './App';
+ 
+ 
+ 
+ AppRegistry.registerComponent('main', () => App);
+ 
